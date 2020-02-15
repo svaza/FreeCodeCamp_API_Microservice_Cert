7a05b4c7b9008781b70a96adbfbda37ea2de80bd
Santosh Vaza <11908578+svaza@users.noreply.github.com>
2020-02-15 18:22:58 +0530

    file analyze api

---
+++ TimestampMicroservice/Controllers/FileAnalyseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TimestampMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileAnalyseController : ControllerBase
+    {
+        
+        public IActionResult Post(IFormFile upfile)
+        {
+            return Ok(new { name = upfile.FileName, type = upfile.ContentType, size = upfile.Length });
+        }
+
+    }
+}
