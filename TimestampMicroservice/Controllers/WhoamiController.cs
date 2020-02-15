d0a63da92c8126c5cc87383edfc955352ec6a721
Santosh Vaza <11908578+svaza@users.noreply.github.com>
2020-02-15 16:16:12 +0530

    add whoami controller

---
+++ TimestampMicroservice/Controllers/WhoamiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TimestampMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WhoamiController : ControllerBase
+    {
+        [Route("")]
+        public IActionResult Get()
+        {
+            return Ok(new { ipaddress = Request.HttpContext.Connection.RemoteIpAddress.ToString(), language = Request.Headers["Accept-Language"], software = Request.Headers["User-Agent"] });
+        }
+    }
+}
